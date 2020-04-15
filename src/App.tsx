2e10b2dd226bd16dfe8d1c021419d4d9e8a45519
Eddy Kim <56513320+Eddy-orbs@users.noreply.github.com>
2020-04-15 16:00:06 +0900

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,85 @@
+import React from 'react';
+import Web3 from 'web3';
+
+function App() {
+  var provider = (window as any).ethereum;
+  var web3 = new Web3(provider);
+
+  if (provider.isStatus) {
+    console.log("kimed");
+  } else if (provider.isMetaMask) {
+    console.log("provider Metamask");
+  } else {
+    console.log("no provider");
+  }
+
+  web3.eth.getAccounts().then(accounts => {
+    web3.eth.defaultAccount = accounts[0];
+  });
+
+  window.addEventListener('load', async () => {
+    // Read-only provider is exposed by default
+    //console.log(await provider.send('net_version'));
+    try {
+        // Request full provider if needed
+        //await provider.enable();
+        // Full provider exposed
+        //await provider.send('eth_sendTransaction', [/* ... */]);
+        console.log("user accepted");
+
+        if (web3.eth.defaultAccount) {
+          console.log("kimed3", web3.eth.defaultAccount);
+        }
+      
+    } catch (error) {
+        // User denied full provider access
+        console.log("user rejected");
+    }
+  });
+
+  return (
+    <div className="App">
+      <p>
+        <a href="/" onClick={function(e){
+          var provider = (window as any).ethereum;
+          provider.enable();
+        }}>click to connect</a>
+      </p>
+      <p>
+
+        <a href="/" onClick={function(e){
+          console.log(web3.eth.defaultAccount);
+          alert(web3.eth.defaultAccount);
+          //e.preventDefault();
+        }}>click to see address</a>
+      </p>
+
+      <p>
+        <a href="/" onClick={function(e){
+          var gasLimit = '15';
+          var account = web3.eth.defaultAccount;
+          console.log(web3.eth.defaultAccount);
+          alert(account);
+
+          if (account) {
+            web3.eth.sendTransaction({
+              from: account,
+              to: '0x11f4d0A3c12e86B4b5F39B213F7E19D048276DAe',
+              value: '1000000000000000',
+              gas: gasLimit // use web3.eth.estimateGas() to get this amount, and add a little for extra safety.
+            }).then(receipt => {
+              console.log((receipt as any).transactionHash); // "0xe7991ac8107a2dd70f996ea0cd867a828b2f228b39436506271d6a53587eff16"
+            }).catch(function(err){
+              console.error(err);
+            });
+          } else {
+            console.log('no account');
+          }
+          e.preventDefault();
+        }}>send Transaction</a>
+      </p>
+    </div>
+  );
+}
+
+export default App;
